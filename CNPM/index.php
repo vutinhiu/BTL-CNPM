aa9ac61488b54693b20095d73073d0083f2e2c7c
nguyencanh2000 <1851161689@e.tlu.edu.vn>
2021-05-29 22:44:34 +0700

    fix

--- CNPM/index.php
+++ CNPM/index.php
@@ -95,20 +95,7 @@
     </header> <!-- section-header.// -->
 <?php include_once('modules/menu/menu.php');?>
 
-<!-- ========================= SECTION MAIN ========================= -->
-<section class="section-main bg padding-y">
-    <div class="container">
 
-        <div class="row">
-           <!-- menuleft -->
-           <?php
-           include_once('modules/menu/menuleft.php');
-           include_once('modules/banner/banner.php');
-           ?>
-        </div> <!-- row.// -->
-    </div> <!-- container //  -->
-</section>
-<!-- ========================= SECTION MAIN END// ========================= -->
 
 <!-- ========================= SECTION  ========================= -->
 <!--Product  -->
@@ -124,7 +111,7 @@
 			case "register": include_once('modules/login/register.php'); break;
         }
     }else{
-		 
+		include_once('modules/menu/menu2.php');
 		include_once('modules/product/featured.php');
 		include_once('modules/product/latest.php');
 	}
